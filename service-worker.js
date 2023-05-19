9a7d2ae0f2750f699a1b76525e5bccfa1bbf08b8
HaraDesu <32463794+HaraDesu@users.noreply.github.com>
2023-05-19 13:41:30 +0700

    Update service-worker.js
    thanks AI.

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,9 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform the install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -24,11 +22,25 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Remove outdated caches
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
   );
 });
